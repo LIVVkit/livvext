410d9939a6682c9c85ab65a316191d580efb352b
Michael Kelleher <kelleher.mike@gmail.com>
2025-12-22 12:53:03 -0800

    Use mache to detect machine and fill in e3sm diags path

--- lex/generate_cfg.py
+++ lex/generate_cfg.py
@@ -4,6 +4,7 @@ import argparse
 from pathlib import Path
 
 import jinja2
+import mache
 
 ALL_SHEETS = "run_gis,run_ais"
 ALL_SETS = "set_cmb,set_smb,set_energy_racmo,set_energy_era5,set_energy_merra2,set_energy_ceres"
@@ -20,27 +21,29 @@ def args():
         type=Path,
         help="Path to configuration template file.",
     )
-    parser.add_argument(
-        "--mach",
-        "-m",
-        type=str,
-        default="pm-cpu",
-        help="Name of the machine to be used. [pm-cpu, chrys]",
-    )
+
     parser.add_argument(
         "--case",
         "-c",
         type=str,
         help="Case ID",
+        required=True,
     )
+
     parser.add_argument(
         "--casedir",
         "-d",
         type=Path,
         help="Output directory where climatology files for `case` are stored",
+        required=True,
     )
+
     parser.add_argument(
-        "--cfg_out", "-o", type=Path, help="Output directory for config file."
+        "--cfg_out",
+        "-o",
+        type=Path,
+        help="Output directory for config file.",
+        default=Path("./").resolve(),
     )
 
     parser.add_argument(
@@ -97,42 +100,46 @@ def parse_sets(sheets, sets):
     _sheets = sheets.lower().split(",")
     _sets = sets.lower().split(",")
 
-    for _sheet in _sheets:
+    sheets = [_sheet.strip() for _sheet in _sheets]
+    sets = [_set.strip() for _set in _sets]
+
+    for _sheet in sheets:
         params[_sheet] = True
 
-    for _set in _sets:
+    for _set in sets:
         params[_set] = True
     return params
 
 
 def main():
     cl_args = args()
+    mach = mache.discover_machine()
+    mach_info = mache.MachineInfo()
+
     defaults = {
         "chrys": {
-            "e3sm_diags_data_dir": Path(
-                "/lcrc/group/e3sm/diagnostics/observations/Atm/"
-            ),
             "livvproj_dir": Path("/lcrc/group/e3sm/livvkit"),
             "model_ts_dir": Path("/lcrc/group/e3sm/ac.zender/scratch/livvkit"),
             "grid_dir": Path("/lcrc/group/e3sm/zender/grids"),
         },
         "pm-cpu": {
-            "e3sm_diags_data_dir": Path(
-                "/global/cfs/cdirs/e3sm/e3sm_diags/obs_for_e3sm_diags"
-            ),
             "livvproj_dir": Path("/global/cfs/cdirs/e3sm/livvkit"),
             "model_ts_dir": Path("/global/cfs/projectdirs/e3sm/zender/livvkit"),
             "grid_dir": Path("/global/cfs/cdirs/e3sm/zender/grids"),
             "racmo_root_dir": Path("/global/cfs/cdirs/fanssie/racmo/2.4.1"),
         },
     }
+    _mach_defaults = defaults[mach]
+    _mach_defaults["e3sm_diags_data_dir"] = Path(
+        mach_info.config.get("diagnostics", "base_path")
+    )
     params = {
-        **defaults[cl_args.mach],
+        **_mach_defaults,
         "case_id": cl_args.case,
         "case_out_dir": cl_args.casedir,
         **parse_sets(cl_args.icesheets, cl_args.sets),
     }
-    out_cfg = Path(cl_args.cfg_out, cl_args.case, "livvkit.yml")
+    out_cfg = Path(cl_args.cfg_out, "livvkit.yml")
     gen_cfg(cl_args.template, params, out_cfg)
 
 
